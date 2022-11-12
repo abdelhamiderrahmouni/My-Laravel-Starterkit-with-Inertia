dd4092339979385484320cf0c10c6cd73e3402c7
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2022-11-12 13:09:33 +0100

    initial commit

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex h-screen bg-gray-200 font-roboto">
+        <Navigation />
+
+        <div class="flex flex-1 flex-col overflow-hidden">
+            <Header>
+                <template #page_title>
+                    <slot name="page_title" />
+                </template>
+            </Header>
+
+            <main class="flex-1 overflow-y-auto overflow-x-hidden bg-gray-200">
+                <div class="container mx-auto px-6 py-4">
+                    <h3 class="mb-4 font-medium text-gray-700">
+                        <slot name="header" />
+                    </h3>
+
+                    <slot />
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Header from '@/Layouts/Header.vue';
+import Navigation from '@/Layouts/Navigation.vue';
+</script>
---
+++ resources/js/Layouts/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <header class="flex items-center justify-between border-b-2 border-indigo-600 bg-white px-6 py-4">
+        <div class="flex items-center gap-4">
+            <button @click="$page.props.showingMobileMenu = !$page.props.showingMobileMenu" class="text-gray-500 focus:outline-none lg:hidden">
+                <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+
+            <slot name="page_title" />
+        </div>
+
+        <div class="flex items-center">
+            <dropdown>
+                <template #trigger>
+                    <button @click="dropdownOpen = ! dropdownOpen" class="relative block overflow-hidden flex gap-2">
+                        {{ $page.props.auth.user.name }}
+
+                        <!-- down chevron-->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                        <!-- down chevron-->
+                    </button>
+                </template>
+
+                <template #content>
+                    <dropdown-link :href="route('profile.show')">
+                        My profile
+                    </dropdown-link>
+
+                    <dropdown-link class="w-full text-left" :href="route('logout')" method="post" as="button">
+                        Log out
+                    </dropdown-link>
+                </template>
+            </dropdown>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+</script>
